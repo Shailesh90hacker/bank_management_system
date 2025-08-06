ecf390d8937ba1dfd072dece502e48e29655e04a
Shailesh Chand <96754898+Shailesh90hacker@users.noreply.github.com>
2025-08-07 04:33:07 +0530

    Update Conn.java

--- src/bank/management/system/Conn.java
+++ src/bank/management/system/Conn.java
@@ -1,5 +1,7 @@
 package bank.management.system;
 
+//step 1 -> Importing required packages
+//step 2 -> Load JDBC Drivers  ( In Modern JDBC (Java 6+), this step is optional.  MySQL JDBC driver auto-registers itself.)
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -10,12 +12,16 @@ public class Conn {
 
     public Conn(){
         try{
+//step 3 -> Establish a Connection
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Shailesh@786");
+//Step 4: Create Statement
             statement = connection.createStatement();
 
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+// Step 4: Execute SQL Queries
+// Step 5: Process the Result
 
 }
